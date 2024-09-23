5d1111a0b6f47f1303fc6ed261ede37923ece680
Kah Ho <sweekahho@gmail.com>
2024-09-23 13:51:25 +0800

    make sidebar collapsible and add simulated wind speed

--- src/pages/Home/Sidebar.tsx
+++ src/pages/Home/Sidebar.tsx
@@ -1,10 +1,13 @@
-import { Button, Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Text, Box } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 // @ts-expect-error no type definition available
 import CanvasJSReact from '@canvasjs/react-charts'
 
 const Sidebar = () => {
 	const [autoStatus, setAutoStatus] = useState(false)
+	const [sidebarOpen, setSidebarOpen] = useState(false)
+	const [latestWindSpeed, setLatestWindSpeed] = useState(9)
+	const [updater, setUpdater] = useState(true)
 	const [windData, setWindData] = useState([
 		{ x: 1, y: 6 },
 		{ x: 2, y: 5 },
@@ -49,16 +52,23 @@ const Sidebar = () => {
 	}
 
 	useEffect(() => {
-		const newData = windData
-		const lastPoint = windData[19]
-		lastPoint.y += 1
-		newData.shift()
-		newData.push(lastPoint)
+		// const newData = windData
+		// const lastPoint = windData[19]
+		// lastPoint.y += 1
+		// newData.shift()
+		// newData.push(lastPoint)
 		setTimeout(() => {
-			setWindData(newData)
-			console.log(newData)
-		}, 1500)
-	}, [windData])
+			// setWindData(newData)
+			const t = Math.floor(Math.random() * 9)
+			if (t < 3 && latestWindSpeed + t - 3 > 0) {
+				setLatestWindSpeed(latestWindSpeed + t - 3)
+			} else if (t > 5 && latestWindSpeed + t - 5 < 20) {
+				setLatestWindSpeed(latestWindSpeed + t - 5)
+			}
+			console.log(latestWindSpeed)
+			setUpdater(!updater)
+		}, 800)
+	}, [updater])
 
 	return (
 		<Flex
@@ -67,17 +77,58 @@ const Sidebar = () => {
 			right="20px"
 			width="300px"
 			borderRadius="10px"
-			bgColor="rgba(255,255,255, 0.85)"
-			boxShadow="lg"
-			padding="14px"
+			bgColor={
+				sidebarOpen ? 'rgba(255,255,255, 0.85)' : 'rgba(255,255,255, 0)'
+			}
+			boxShadow={sidebarOpen ? 'lg' : 'none'}
+			padding="12px 20px 20px 20px"
 			flexDir="column"
 			gap="12px"
 		>
+			<Flex justifyContent="end">
+				<Button
+					height="50px"
+					width="50px"
+					bgColor="white"
+					border="4px solid #FFB030"
+					borderRadius="100%"
+					boxShadow="lg"
+					flexDir="column"
+					gap="4px"
+					alignItems="center"
+					justifyContent="center"
+					padding="2px"
+					onClick={() => setSidebarOpen(!sidebarOpen)}
+				>
+					<Box
+						width="20px"
+						height="3px"
+						border="1px solid black"
+						borderRadius="8px"
+						bgColor="black"
+					/>
+					<Box
+						width="20px"
+						height="3px"
+						border="1px solid black"
+						borderRadius="8px"
+						bgColor="black"
+					/>
+					<Box
+						width="20px"
+						height="3px"
+						border="1px solid black"
+						borderRadius="8px"
+						bgColor="black"
+					/>
+				</Button>
+			</Flex>
 			<Flex
 				justifyContent="center"
 				borderRadius="4px"
 				bgColor="white"
 				padding="10px"
+				display={sidebarOpen ? 'flex' : 'none'}
 			>
 				<Text>{riskLevel}</Text>
 			</Flex>
@@ -88,9 +139,10 @@ const Sidebar = () => {
 				borderRadius="4px"
 				bgColor="#FFDE70"
 				padding="20px"
+				display={sidebarOpen ? 'flex' : 'none'}
 			>
 				<Text fontSize="3xl" fontWeight="bold">
-					{windData[0].y} Knots
+					{latestWindSpeed} Knots
 				</Text>
 				<Text>Wind Speed</Text>
 			</Flex>
@@ -100,6 +152,7 @@ const Sidebar = () => {
 				justifyContent="center"
 				borderRadius="4px"
 				padding="20px"
+				display={sidebarOpen ? 'flex' : 'none'}
 			>
 				{/* <CanvasJSChart options={options} /> */}
 			</Flex>
@@ -110,6 +163,7 @@ const Sidebar = () => {
 				width="100%"
 				minHeight="60px"
 				_hover={{ bgColor: '#5A5A5A' }}
+				display={sidebarOpen ? 'flex' : 'none'}
 			>
 				{autoStatus ? 'Disable Auto-Berthing' : 'Enable Auto-Berthing'}
 			</Button>
